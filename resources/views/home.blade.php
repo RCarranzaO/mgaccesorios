636accc5f9e0a1d8c09d84ec9d80b2edd5ddf28f
Rcarranza13 <rcarranza1390@gmail.com>
2018-05-19 19:59:55 -0500

    Vista de la plantilla lista, solo hayq eu hacer las funciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container flex-center position-ref full-height">
+<div class="flex-center position-ref full-height">
     <div class="row justify-content-center">
         <div class="col-md-8">
-          <div class="content">
-              <div class="title m-b-md">
-
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                  Mg Accesorios
-              </div>
-          </div>
-
+            <div class="content">
+                <div class="title ">
+                      @if (session('status'))
+                          <div class="alert alert-success">
+                              {{ session('status') }}
+                          </div>
+                      @endif
+                    Mg Accesorios
+                </div>
+            </div>
         </div>
     </div>
 </div>
 @endsection
-<!--<div class="card">
-    <div class="card-header">Dashboard</div>
-
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        You are logged in!
-    </div>
-</div>-->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'MgAccesorios') }}</title>
+    <title>MgAccesorios</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -67,14 +67,15 @@
         }
 
         .m-b-md {
-            margin-bottom: 30px;
+            height:  637px;
         }
+
     </style>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="m-b-md" id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel mr-auto">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     MgAccesorios
@@ -87,8 +88,66 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav nav-link links">
                         <a href="{{url('/home')}}">Inicio </a>
-                    </ul>
 
+                        @guest
+
+                        @else
+                            <li class="nav-item dropdown links" >
+                                <a class="nav-link dropdown-toggle" id="navbarDropdowncajaLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Caja</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdowncajaLink">
+                                    <a class="dropdown-item" href="#">Fondo</a>
+                                    <a class="dropdown-item" href="#">Egreso</a><!--Aqui es la tabla de gastos-->
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown links">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown">Entradas</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Compras</a>
+                                    <a class="dropdown-item" href="#">Devoluciones</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown links">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown">Salidas</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Ventas</a>
+                                    <a class="dropdown-item" href="#">Traspaso a Sucursal</a>
+                                    <a class="dropdown-item" href="#">Salida especial</a>
+                                </div>
+                            </li>
+                            @if (Auth::user()->rol == 1)
+                                <li class="nav-item dropdown links">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown">Productos</a>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Alta Producto</a>
+                                        <a class="dropdown-item" href="#">Modificar Porducto</a>
+                                        <a class="dropdown-item" href="#">Baja Producto</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown links">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown">Usuarios</a>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Alta Usuario</a>
+                                        <a class="dropdown-item" href="#">Modificar Usuario</a>
+                                        <a class="dropdown-item" href="#">Baja Usuario</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown links">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown">Sucursal</a>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Alta Sucursal</a>
+                                        <a class="dropdown-item" href="#">Baja Sucursal</a>
+                                    </div>
+                                </li>
+                            @endif
+                            <li class="nav-item dropdown links">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown">Reportes</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Venta</a>
+                                    <a class="dropdown-item" href="#">Inventario</a>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -96,6 +155,7 @@
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                             <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                         @else
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->username }} <span class="caret"></span>
@@ -118,10 +178,10 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
+        <main class="py-4 m-b-md">
             @yield('content')
         </main>
+        </div>
     </div>
 </body>
 </html>
