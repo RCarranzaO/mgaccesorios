6fff7bce1f55db38ef4bc704f5d7dff0c8989ee3
DNAPhoenix <dan.akagami@gmail.com>
2018-05-23 18:14:15 -0500

    Checando baja de usuarios

---
+++ resources/views/usuario/baja.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+	<table class="table text-center">
+		<thead class="thead-dark">
+			<tr>
+				<th scope="col">ID</th>
+				<th scope="col">Nombre</th>
+				<th scope="col">Apellido</th>
+				<th scope="col">Estatus</th>
+				<th scope="col">ID</th>
+			</tr>
+		</thead>
+	</table>
+</div>
+
+@endsection
