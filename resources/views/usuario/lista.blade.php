4239232bd431037282ab2cd94a4199369136638e
DNAPhoenix <dan.akagami@gmail.com>
2018-05-26 11:07:42 -0500

    Cambios en lista.blade.php se agregó campo de dar baja y se aceptan ver a todos los usuarios, no solo los que estan en alta.

--- resources/views/usuario/lista.blade.php
+++ resources/views/usuario/lista.blade.php
@@ -12,7 +12,9 @@
 				<th scope="col">Usuario</th>
 				<th scope="col">Correo</th>
 				<th scope="col">Rol</th>
+				<th scope="col">Estatus</th>
 				<th scope="col">Editar</th>
+				<th scope="col">Dar de baja</th>
 			</tr>
 		</thead>
 
@@ -20,25 +22,35 @@
 			
 			@foreach($usuarios as $usuario)
 
-				@if($usuario->estatus==1)
-					<tr>
-						<td>{{ $usuario->id_user }}</td>
-						<td>{{ $usuario->name }}</td>
-						<td>{{ $usuario->lastname }}</td>
-						<td>{{ $usuario->username }}</td>
-						<td>{{ $usuario->email }}</td>
-						<td>
-							@if($usuario->rol == 1)
-								{{ $rol = 'Administrador' }}
-							@else
-								{{ $rol = 'Vendedor' }}
-							@endif
-						</td>
-						<td>
-							<a href="{{ route('usuario.edit', $usuario->id_user) }}" class="btn btn-outline-info">Editar</a>
-						</td>
-					</tr>
-				@endif
+
+				<tr>
+					<td>{{ $usuario->id_user }}</td>
+					<td>{{ $usuario->name }}</td>
+					<td>{{ $usuario->lastname }}</td>
+					<td>{{ $usuario->username }}</td>
+					<td>{{ $usuario->email }}</td>
+					<td>
+						@if($usuario->rol == 1)
+							{{ $rol = 'Administrador' }}
+						@else
+							{{ $rol = 'Vendedor' }}
+						@endif
+					</td>
+					<td>
+						@if($usuario->estatus == 1)
+							{{ $rol = 'Activo' }}
+						@else
+							{{ $rol = 'Inactivo' }}
+						@endif
+					</td>
+					<td>
+						<a href="{{ route('usuario.edit', $usuario->id_user) }}" class="btn btn-outline-info">Editar</a>
+					</td>
+					<td>
+						<a href="{{ route('usuario.edit', $usuario->id_user) }}" class="btn btn-outline-danger">Baja</a>
+					</td>
+				</tr>
+
 
 			@endforeach
 		</tbody>
