73ac81cb639bf88bd237f3961a821a97a700fb72
Rcarranza13 <rcarranza1390@gmail.com>
2018-05-31 00:27:02 -0500

    actualizacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,59 +13,9 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <!--<link href=" asset('css/app.css') }}" rel="stylesheet">-->
+        <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
