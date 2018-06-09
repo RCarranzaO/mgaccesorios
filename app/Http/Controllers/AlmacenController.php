7a10c5deac299a509b3c2e6af206366d69f314e4
Rcarranza13 <rcarranza1390@gmail.com>
2018-06-09 18:19:09 -0500

    almacen

--- app/Http/Controllers/AlmacenController.php
+++ app/Http/Controllers/AlmacenController.php
@@ -4,23 +4,89 @@ namespace mgaccesorios\Http\Controllers;
 
 use Illuminate\Http\Request;
 use mgaccesorios\Http\Controllers\Controller;
-use mgaccesorios\Producto;
-use mgaccesorios\Sucursal;
 
 class AlmacenController extends Controller
 {
-    public function __construct()
-    {
-       $this->middleware('auth');
-    }
-    public function index()
-    {
-        $productos = Producto::all();
-        return view('almacen.almacen', compact('productos'));
-    }
-    public function show()
-    {
-        
-    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 
+
+      public function __construct()
+      {
+          $this->middleware('auth');
+      }
+      public function index()
+      {
+          $almacenes = DetalleAlmacen::all();
+          return view('almacen.almacen', compact('almacenes'));
+      }
+
+      /**
+       * Show the form for creating a new resource.
+       *
+       * @return \Illuminate\Http\Response
+       */
+      public function create()
+      {
+          //
+      }
+
+      /**
+       * Store a newly created resource in storage.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @return \Illuminate\Http\Response
+       */
+      public function store(Request $request)
+      {
+          //
+      }
+
+      /**
+       * Display the specified resource.
+       *
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function show($id)
+      {
+          //
+      }
+
+      /**
+       * Show the form for editing the specified resource.
+       *
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function edit($id)
+      {
+          //
+      }
+
+      /**
+       * Update the specified resource in storage.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function update(Request $request, $id)
+      {
+          //
+      }
+
+      /**
+       * Remove the specified resource from storage.
+       *
+       * @param  int  $id
+       * @return \Illuminate\Http\Response
+       */
+      public function destroy($id)
+      {
+          //
+      }
 }
