91b113d65132af0fd65ab16fb9729b5574ca0f04
Rcarranza13 <rcarranza1390@gmail.com>
2018-06-20 17:12:57 -0500

    cambios

--- resources/views/alerts/errores.blade.php
+++ resources/views/alerts/errores.blade.php
@@ -11,4 +11,11 @@
             @endforeach
         </ul>
     </div>
+@elseif (session('fail'))
+  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+      </button>
+      {{ session('fail') }}
+  </div>
 @endif
