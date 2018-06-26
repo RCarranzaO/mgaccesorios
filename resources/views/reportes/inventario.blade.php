c07167d8f0f356fb81ea36ca28d6a172365a9a1a
DNAPhoenix <dan.akagami@gmail.com>
2018-06-25 19:21:41 -0500

    Se cambió vista de reporte de ventas y de inventario para que los vendedores puedan verlos

--- resources/views/reportes/inventario.blade.php
+++ resources/views/reportes/inventario.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    @if (Auth::user()->rol == 1)
+
         <div class="container">
+            @if (Auth::user()->rol == 1)
             <p>
                 <a href="{{ route('almacen.pdf') }}" class="btn btn-sm btn-primary">
                 Descargar productos en PDF
                 </a>
             </p>
+            @endif
             <table class="table text-center table-responsive-sm">
                 <thead class="thead-dark">
                     <tr>
@@ -46,33 +48,5 @@
             </table>
             <hr>
         </div>
-    @else
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function()
-            {
-                $("#myModal").modal("show");
-            });
-        </script>
-        <div class="modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title"  id="exampleModalLabel">Error</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <h3>No tienes lo privilegios para entrar aquí!! Adios!</h3>
-                    </div>
-                    <div class="modal-footer">
-                        <form class="" action="{{ route('home') }}">
-                            <button type="submit" class="btn btn-outline-primary">Cerrar</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endif
+
 @endsection
