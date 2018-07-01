2dfdbecef292dd0751b61427db706e1efaeabe47
DNAPhoenix <dan.akagami@gmail.com>
2018-06-30 19:15:40 -0500

    Alerta en el blade welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
 
     </head>
     <body>
+        @include('alerts.errores')
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
