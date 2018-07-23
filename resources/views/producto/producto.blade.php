42abeff936da12846c744c7dae3b6defa14a7b01
DNAPhoenix <dan.akagami@gmail.com>
2018-07-22 20:56:04 -0500

    Busqueda en productos ya funciona

--- resources/views/producto/producto.blade.php
+++ resources/views/producto/producto.blade.php
@@ -6,6 +6,7 @@
           <nav class="navbar navbar-ligth bg-ligth justify-content-left ">
             <form class="form-inline" action="#" method="get">
               <input type="text" id="buscar" class="form-control mr-sm-2" name="buscar" placeholder="Buscar producto">
+              {{ $productos->links() }}
             </form>
           </nav>
           <table class="table text-center table-responsive-sm">
@@ -41,32 +42,31 @@
                                   <a href="{{ route('producto.edit', $producto->id_producto) }}" class="btn btn-outline-info">Editar</a>
                               </td>
                               <td>
-                                  <form method="post" action="/producto/{{ $producto->id_producto }}" >
+                                  <button type="button" class="{{ $producto->estatus==1 ? 'btn btn-outline-danger' : 'btn btn-outline-success' }}" data-toggle="modal" data-target="#ModalDelete{{$producto->id_producto}}">{{ $producto->estatus == 1 ? _('Baja') : _('Alta') }}</button>
+                                  <form method="post" action="/producto/{{ $producto->id_producto }}">
                                       @csrf
                                       @method('DELETE')
-                                      <button type="button" class="{{ $producto->estatus==1 ? 'btn btn-outline-danger' : 'btn btn-outline-success' }}" data-toggle="modal" data-target="#ModalDelete{{$producto->id_producto}}">{{ $producto->estatus == 1 ? _('Baja') : _('Alta') }}</button>
                                       <div class="modal fade" id="ModalDelete{{$producto->id_producto}}" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
                                           <div class="modal-dialog" role="document">
                                               <div class="modal-content">
                                                   <div class="modal-header">
-                                                      <h5 class="modal-title" id="ModalLabel">Alerta!</h5>
+                                                      <h5 class="modal-title" id="ModalLabel">{{ '¡Alerta!' }}</h5>
                                                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                           <span aria-hidden="true">&times;</span>
                                                       </button>
                                                   </div>
                                                   <div class="modal-body">
-                                                      <h3>{{ $producto->estatus==1 ? 'Desea dar de baja el producto?' : 'Desea dar de alta el producto?' }}</h3>
+                                                      <h3>{{ $producto->estatus==1 ? '¿Desea dar de baja el producto?' : '¿Desea dar de alta el producto?' }}</h3>
                                                   </div>
                                                   <div class="modal-footer">
-                                                          <!--<a class="btn btn-outline-primary" href="">Aceptar</button>-->
-                                                          <button type="submit" class="btn btn-outline-primary">Aceptar</button>
-                                                          <button type='button' class="btn btn-outline-secondary" data-dismiss="modal">Cancelar</button>
+                                                      <button type="submit" class="btn btn-outline-primary">{{ 'Aceptar' }}</button>
+                                                      <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">{{ 'Cancelar' }}</button>
                                                   </div>
                                               </div>
                                           </div>
                                       </div>
                                   </form>
-                  						</td>
+                              </td>
                           </tr>
 
                       @endforeach
@@ -80,11 +80,6 @@
               </tbody>
           </table>
           <hr>
-          <div class="row">
-            <div class="col-ms-1">
-              {{ $productos->links() }}
-            </div>
-          </div>
 
       </div>
   @else
