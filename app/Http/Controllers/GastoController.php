0dad81601d65649113f6fee90cad5353b2179df1
Rcarranza13 <rcarranza1390@gmail.com>
2018-07-29 13:41:50 -0500

    arreglo de retiro

--- app/Http/Controllers/GastoController.php
+++ app/Http/Controllers/GastoController.php
@@ -10,7 +10,7 @@ use mgaccesorios\Saldo;
 
 class GastoController extends Controller
 {
-    
+
     /**
      * La función function_construct se encarga de verificar que el usuario ha iniciado sesión antes de poder realizar cualquier acción.
      * @return type
@@ -19,7 +19,7 @@ class GastoController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     /**
      * La función index hace una llamada al archivo gasto.blade.php
      * @return Devuelve la vista del formulario para realizar un retiro de dinero de la caja.
@@ -33,7 +33,7 @@ class GastoController extends Controller
     /**
      * La función saveGasto guarda la información de la cantidad de dinero que se va a retirar. Se debe especificar la cantidad y una descripción del porque se realiza dicho retiro de caja. El retiro se registra como un nuevo gasto, se resta del fondo de caja y se actualiza el fondo. Se guarda la información en la base de datos junto con la fecha en que se registra el retiro.
      * No es posible realizar un retiro mayor al saldo actual en caja ni realizar un retiro de dinero que deje el saldo de caja con menos de 500 pesos.
-     * @param Los parámetros requeridos para el gaso son cantidad de tipo numérico y descripción de tipo string con un vaor máximo de 255 caracteres. 
+     * @param Los parámetros requeridos para el gaso son cantidad de tipo numérico y descripción de tipo string con un vaor máximo de 255 caracteres.
      * @return Si el retiro no se lleva acabo devuelve un mensaje de error indicando que la cnatidad que se desea retirar no está permitida.
      * Si el monto total a retirar es válido se guarda la información y nos redirecciona a la página principal del sistema Mg Accesorios.
      */
@@ -51,19 +51,23 @@ class GastoController extends Controller
         $date = Carbon::now();
         $date = $date->toDateString();
 
-        if ($request->input('cantidad') >=0 && $request->input('cantidad') <= $saldoId->saldo_actual) {
-            if (($saldoId->saldo_actual - $request->input('cantidad')) < 500) {
-                return redirect()->route('gasto')->with('fail', 'La cantidad a retirar no está permitida!');
+        if ($saldoId->fecha == $date) {
+            if ($request->input('cantidad') >=0 && $request->input('cantidad') <= $saldoId->saldo_actual) {
+                if (($saldoId->saldo_actual - $request->input('cantidad')) < 500) {
+                    return redirect()->route('gasto')->with('fail', 'La cantidad a retirar no está permitida!');
+                } else {
+                    $gastos->id_fondo = $fondoId->id_fondo;
+                    $gastos->descripcion = $request->input('descripcion');
+                    $gastos->cantidad = $request->input('cantidad');
+                    $gastos->fecha = $date;
+                    $gastos->save();
+                    return redirect()->route('guardarGasto');
+                }
             } else {
-                $gastos->id_fondo = $fondoId->id_fondo;
-                $gastos->descripcion = $request->input('descripcion');
-                $gastos->cantidad = $request->input('cantidad');
-                $gastos->fecha = $date;
-                $gastos->save();
-                return redirect()->route('guardarGasto');
+                return redirect()->route('gasto')->with('fail', 'La cantidad a retirar no está permitida!');
             }
         } else {
-            return redirect()->route('gasto')->with('fail', 'La cantidad a retirar no está permitida!');
+            return redirect()->route('fondo')->with('fail', 'No se ha registrado un fondo el dia de hoy');
         }
 
     }
