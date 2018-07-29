430e7ade68b3793506d2debfab3fb104ddcded3c
DNAPhoenix <dan.akagami@gmail.com>
2018-07-29 16:15:18 -0500

    Correo para restablecer password cambiado

--- resources/views/email/password.blade.php
+++ resources/views/email/password.blade.php
@@ -46,7 +46,7 @@
                                 <!-- Body content -->
                                 <tr>
                                     <td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                                        <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">Hello!</h1>
+                                        <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">¡Hola!</h1>
 <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">You are receiving this email because we received a password reset request for your account.</p>
 <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; padding: 0; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
     <tr>
@@ -68,12 +68,11 @@
     </tr>
 </table>
 <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">If you did not request a password reset, no further action is required.</p>
-<p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Regards,<br>Laravel</p>
+<p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Saludos,<br>MgAccesorios</p>
 <table class="subcopy" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-top: 1px solid #EDEFF2; margin-top: 25px; padding-top: 25px;">
     <tr>
         <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-            <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; line-height: 1.5em; margin-top: 0; text-align: left; font-size: 12px;">If you’re having trouble clicking the "Reset Password" button, copy and paste the URL below
-into your web browser: <a href="http://localhost/password/reset/f4e97857974eb4fe8ec677fca971da284340e8cd1d90b23cf76a59620f451ea5" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">http://localhost/password/reset/f4e97857974eb4fe8ec677fca971da284340e8cd1d90b23cf76a59620f451ea5</a></p>
+            <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; line-height: 1.5em; margin-top: 0; text-align: left; font-size: 12px;">Si tiene problemas con el botón "Restablecer Contraseña", copie y pegue el enlace de abajo en su navegador web: <a href="http://localhost/password/reset/f4e97857974eb4fe8ec677fca971da284340e8cd1d90b23cf76a59620f451ea5" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">http://localhost/password/reset/f4e97857974eb4fe8ec677fca971da284340e8cd1d90b23cf76a59620f451ea5</a></p>
         </td>
     </tr>
 </table>
@@ -90,7 +89,7 @@ into your web browser: <a href="http://localhost/password/reset/f4e97857974eb4fe
         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
             <tr>
                 <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #AEAEAE; font-size: 12px; text-align: center;">© 2018 Laravel. All rights reserved.</p>
+                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #AEAEAE; font-size: 12px; text-align: center;">© 2018 MgAccesorios. Todos los derechos reservados.</p>
                 </td>
             </tr>
         </table>
